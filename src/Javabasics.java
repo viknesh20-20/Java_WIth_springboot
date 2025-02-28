dc8c893ce4acb7c13122bc0a10f6e2180b9531bf
ACER <aitester2005@gmail.com>
2025-02-28 12:09:17 +0530

    First update

---
+++ src/Javabasics.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Javabasics {
+    public static void main(String[] args) {
+
+        int a;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        System.out.println(a);
+
+
+//        short sh = 1234;
+//        byte bv = 12;
+//        long lg = 12365463845678932l;
+//        float flt = 2.32f;
+//        double dbl = 54.67512354245;
+//        char chr = 's';
+//        boolean bool = true;
+
+
+//        String tmp = "Value";
+//        System.out.println(tmp);
+
+
+//        System.out.println("Java");
+//        System.out.print("is a");
+//        System.out.println("Programming language");
+//        System.out.println(4);
+//        System.out.println(4.5);
+    }
+}
